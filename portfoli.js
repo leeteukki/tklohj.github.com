7056bde37cb28e5508b710951c84984864818d9f
leeteukki <tklohj@naver.com>
2019-02-08 17:23:24 +0900

    add README, js, css, html

--- portfoli.js
+++ portfoli.js
@@ -26,3 +26,36 @@ function menuToggle() {
   document.getElementById('menu').classList.toggle('show');
 }
 document.getElementById('toggleBtn').addEventListener('click', menuToggle);
+
+/*WELCOME AREA */
+
+var imageSlideIndex = 1;
+
+showImageSlides(imageSlideIndex);
+
+function plusImageSlides(n) {
+  showImageSlides(imageSlideIndex += n);
+
+}
+
+function currentImageSlide(n) {
+  showImageSlides(imageSlideIndex = n);
+
+}
+
+ function showImageSlides(n) {
+   var i;
+   var slides = document.getElementsByClassName('image-slide');
+   var dots = document.getElementsByClassName('dot');
+   if(n > slides.length) { imageSlideIndex =1}
+   if(n<1) {imageSlideIndex = slide.length}
+   for(i=0; i < dots.length; i++) {
+     slides[i].style.display = 'none';
+   }
+   for(i=0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(' active', "");
+
+   }
+   slides[imageSlideIndex - 1].style.display = 'block';
+   dots[imageSlideIndex - 1 ].className = 'active';
+ }
