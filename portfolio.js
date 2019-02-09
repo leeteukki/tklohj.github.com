b6ce4842841cbd81872f8c2f3e9485d6388e7c4e
leeteukki <tklohj@naver.com>
2019-02-09 21:40:08 +0900

    update code js file

--- portfolio.js
+++ portfolio.js
@@ -1,34 +1,35 @@
-/* Header */
-window.onload = function() { scorllFunction()};
-window.onscroll = function() {scorllFunction()};
+/* HEADER */
+window.onload = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()};
 
-function scorllFunction() {
-  var header = document.getElementById('header')
+function scrollFunction() {
+    var header = document.getElementById('header');
 
-  if (document.documentElement.scrollTop > 70) {
-    if(!header.classList.contains('navbar-fixed')) {
-      header.classList.add('navbar-fixed')
-      document.getElementByTagName('body')[0].style.marginTop = '70px';
-      header.style.display = 'none'
-      setTimeout(function(){
-        header.style.display = 'block';
-      }, 40);
+    if(document.documentElement.scrollTop > 70) {
+        if(!header.classList.contains('navbar-fixed')) {
+            header.classList.add('navbar-fixed');
+            document.getElementsByTagName('body')[0].style.marginTop = '70px';
+            header.style.display = 'none';
+            setTimeout(function(){
+                header.style.display = 'block';
+            }, 40);
+        }
+    } else {
+        if(header.classList.contains('navbar-fixed')) {
+            header.classList.remove('navbar-fixed');
+            document.getElementsByTagName('body')[0].style.marginTop = '0';
+        }
     }
-  } else{
-    if(header.classList.contains('navbar-fixed')){
-      header.classList.remove('navbar-fixed');
-      document.getElementByTagName('body')[0].style.marginTop = 0;
-    }
-  }
 }
 
 function menuToggle() {
-  document.getElementById('menu').classList.toggle('show');
+    document.getElementById('menu').classList.toggle('show');
 }
+
 document.getElementById('toggleBtn').addEventListener('click', menuToggle);
 
-/*WELCOME AREA */
 
+/* WELCOME AREA */
 var imageSlideIndex = 1;
 showImageSlides(imageSlideIndex);
 
@@ -40,37 +41,38 @@ var imageTimer = setInterval(imageSlideTimer, 3000);
 
 function plusImageSlides(n) {
   clearInterval(imageTimer);
-
   imageTimer = setInterval(imageSlideTimer, 3000);
 
   showImageSlides(imageSlideIndex += n);
 }
 
-function currentImageSlide(n) {
+function currentImageSlide(n){
   clearInterval(imageTimer);
-  imageTimer = setInterval(imageSlideIndex = n);
+  imageTimer = setInterval(imageSlideTimer, 3000);
 
+  showImageSlides(imageSlideIndex = n);
 }
 
 function showImageSlides(n) {
   var i;
   var slides = document.getElementsByClassName('image-slide');
   var dots = document.getElementsByClassName('dot');
-  if(n > slide.length) {imageSlideIndex = 1}
-  if( n < 1) {imageSlideIndex = slides.length}
-
-  for( i = 0; dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '');
-
+  if (n > slides.length) {imageSlideIndex = 1}
+  if (n < 1) {imageSlideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
   }
-  slides[imageSlideIndex -1].style.display = 'block';
-  dots[imageSlideIndex -1].className += ' active';
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[imageSlideIndex-1].style.display = 'block';
+  dots[imageSlideIndex-1].className += ' active';
 }
 
-document.getElementById('imagePrev').addEventListener('click', plusImageSlides.bind(null, -1));
-document.getElementById('imageNext').addEventListener('click', plusImageSlides.bind(null, 1));
+document.getElementById('imagePrev').addEventListener('click', plusImageSlides.bind(null,-1));
+document.getElementById('imageNext').addEventListener('click', plusImageSlides.bind(null,1));
 
-document.getElementById('firstDot').addEventListener('click', plusImageSlides.bind(null, 1));
-document.getElementById('secondDot').addEventListener('click', plusImageSlides.bind(null, 2));
-document.getElementById('thirdDot').addEventListener('click', plusImageSlides.bind(null, 3));
-document.getElementById('forthDot').addEventListener('click', plusImageSlides.bind(null, 4));
+document.getElementById('firstDot').addEventListener('click', currentImageSlide.bind(null,1));
+document.getElementById('secondDot').addEventListener('click', currentImageSlide.bind(null,2));
+document.getElementById('thirdDot').addEventListener('click', currentImageSlide.bind(null,3));
+document.getElementById('forthDot').addEventListener('click', currentImageSlide.bind(null,4));
